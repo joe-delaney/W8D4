084bc84c0624d49444a4706a2ae4e435cb2fe5be
App Academy Student <email@email.com>
2022-03-24 14:45:13 -0400

    finished callback exercises

---
+++ callbackExercises/myDebounce.js
@@ -0,0 +1,44 @@
+Function.prototype.myDebounce = function(interval) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(this, interval);
+  };
+};
+
+//To be used for testing our debounce function
+class SearchBar {
+  constructor() {
+    this.query = "";
+
+    this.type = this.type.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  type(letter) {
+    this.query += letter;
+    this.search();
+  }
+
+  search() {
+    console.log(`searching for ${this.query}`);
+  }
+}
+
+const searchBar = new SearchBar();
+searchBar.search = searchBar.search.myDebounce(500);
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+queryForHelloWorld();
